919842dd969dbb2c396afd1d9251be24192add46
krishna.rowter@gmail.com <krishna.rowter@gmail.com>
2020-03-12 13:31:12 +0700

    add new numbers

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 window.onload = () => {
   const element = document.getElementById("detail");
 
-  const innerHeight = `Inner height-nya ${window.innerHeight} px`;
-  const innerWidth = `Inner width-nya ${window.innerWidth} px`;
-  const outerHeight = `Inner height-nya ${window.outerHeight} px`;
-  const outerWidth = `Inner width-nya ${window.outerWidth} px  `;
+  const width = `Lebar layar adalah ${window.screen.width} px`;
+  const height = `Tinggi layar adalah ${window.screen.height} px`;
   const userAgent = `User agent: ${navigator.userAgent}`;
   const browserVendor = `Browser vendor-nya ${navigator.vendor}`;
 
-  const content = [
-    innerHeight,
-    innerWidth,
-    outerHeight,
-    outerWidth,
-    userAgent,
-    browserVendor
-  ];
+  const content = [width, height, userAgent, browserVendor];
   content.forEach(text => {
     const TextNode = document.createElement("P");
     TextNode.innerHTML = `<h4>${text}</h4>`;

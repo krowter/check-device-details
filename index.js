0342df9f06a57bcb694e8beae59b2f1de3abffbd
krishna.rowter@gmail.com <krishna.rowter@gmail.com>
2020-03-12 10:31:16 +0700

    fix typo

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ window.onload = () => {
   const element = document.getElementById("detail");
 
   const innerHeight = `Inner height-nya ${window.innerHeight} px`;
-  const innerWidth = `Inner height-nya ${window.innerWidth} px`;
+  const innerWidth = `Inner width-nya ${window.innerWidth} px`;
   const outerHeight = `Inner height-nya ${window.outerHeight} px`;
-  const outerWidth = `Inner height-nya ${window.outerWidth} px  `;
+  const outerWidth = `Inner width-nya ${window.outerWidth} px  `;
   const userAgent = `User agent: ${navigator.userAgent}`;
   const browserVendor = `Browser vendor-nya ${navigator.vendor}`;
 

0dff09cfa267001911ce712e42e9b1d31017ad2a
krowter <krishna.rowter@gmail.com>
2020-03-12 10:15:54 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+window.onload = () => {
+  const element = document.getElementById("detail");
+
+  const innerHeight = `Inner height-nya ${window.innerHeight} px`;
+  const innerWidth = `Inner height-nya ${window.innerWidth} px`;
+  const outerHeight = `Inner height-nya ${window.outerHeight} px`;
+  const outerWidth = `Inner height-nya ${window.outerWidth} px  `;
+  const userAgent = `User agent: ${navigator.userAgent}`;
+  const browserVendor = `Browser vendor-nya ${navigator.vendor}`;
+
+  const content = [
+    innerHeight,
+    innerWidth,
+    outerHeight,
+    outerWidth,
+    userAgent,
+    browserVendor
+  ];
+  content.forEach(text => {
+    const TextNode = document.createElement("P");
+    TextNode.innerHTML = `<h4>${text}</h4>`;
+    element.appendChild(TextNode);
+  });
+};
